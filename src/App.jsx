c94eeba1ea73a47f099fdce7df15bf567902e72a
mateokeller <mateokellergms@gmail.com>
2023-01-25 09:54:27 -0300

    add Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import Header from "./ui/Header";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,12 +9,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Header />
       </div>
       <h1>Hello World</h1>
       <div className="card">
---
+++ src/ui/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="header-content">
+        <img className="logo" src="/src/assets/altoloho@150x.png"></img>
+        <div className="header-nav">
+          <a href="">Github</a> <a href="">Portfolio</a> <a href="">About Me</a>
+          <button>Contact Me</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
