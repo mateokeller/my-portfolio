64f505a265f762b0ca2a96b3f62d846c82479c10
mateokeller <mateokellergms@gmail.com>
2023-02-02 13:28:51 -0300

    add heading styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,24 @@ function App() {
     <div className="App">
       <Header />
       <div className="container">
-        <div className="intro"></div>
+        <div className="intro">
+          <p className="group-indicator">&lt;ABOUT ME&gt;</p>
+          <div className="text-container">
+            <h1>
+              MATEO KELLER<span>.</span>
+            </h1>
+            <div className="h-line"></div>
+            <p className="socials-text">
+              <a href="https://www.instagram.com/mateo.keller/">instagram</a> |{" "}
+              <a href="http://www.linkedin.com/in/mateo-keller">LinkedIn</a>
+            </p>
+            <p className="socials-text">
+              <a href="https://github.com/mateokeller">Github</a> |{" "}
+              <a href="">Dribbble</a>
+            </p>
+            <button className="btn">CONTACT ME</button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -6,8 +6,9 @@ const Header = () => {
       <div className="header-content">
         <img className="logo" src="/src/assets/altoloho@150x.png"></img>
         <div className="header-nav">
-          <a href="">Github</a> <a href="">Portfolio</a> <a href="">About Me</a>
-          <a>Contact</a>
+          <a href="https://github.com/mateokeller">Github</a>{" "}
+          <a href="">Portfolio</a> <a href="">About Me</a>
+          <a href="">Contact</a>
         </div>
       </div>
     </div>
