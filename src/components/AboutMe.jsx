65e28113de0018f91963dd2f0119044baa94d43e
Mateo Keller <mateokellergms@gmail.com>
2025-07-30 11:03:11 -0300

    fix cv url

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -23,7 +23,7 @@ const AboutMe = () => {
         </p>
         <a
           className="font-extrabold text-xl no-underline text-black hover:text-yellow-primary mt-8"
-          href="/public/CV_English.pdf"
+          href="/CV_English.pdf"
         >
           RESUME
         </a>
